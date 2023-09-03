c04e6d49794ad021dc17b208e48923ade26695aa
wertzuk <pascalb95@gmx.de>
2023-09-03 19:25:30 +0200

    add basic event component

--- src/App.vue
+++ src/App.vue
@@ -4,28 +4,10 @@
   </header>
   <main class="p-4 text-gray-900">
     <h1 class="text-6xl my-8 font-serif">Januar</h1>
-    <div class="shadow p-4 bg-slate-200 rounded text-slate-800">
-      <div
-        class="text-xl font-thin border-black/20 border-b pb-4 tracking-wider"
-      >
-        01/01/2023 - 03/01/2023
-      </div>
-      <div class="mt-3">
-        <h2 class="text-2xl">Deutsche Blitzmeisterschaft der Amateure</h2>
-        <ul class="mt-4 mb-6 font-thin">
-          <li>München</li>
-          <li>9 Runden</li>
-          <li>2h 40/30 min rest</li>
-        </ul>
-        <button
-          type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
-        >
-          Mehr anzeigen
-        </button>
-      </div>
-    </div>
+    <Event />
   </main>
 </template>
 
-<script setup></script>
+<script setup>
+import Event from './components/Event.vue';
+</script>
---
+++ src/components/Event.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="shadow bg-slate-200 rounded text-slate-800 mb-8">
+    <header class="p-4 pb-0">
+      <div
+        class="text-xl font-thin border-black/20 border-b pb-4 tracking-wider"
+      >
+        01/01/2023 - 03/01/2023
+      </div>
+    </header>
+    <div class="px-4 py-2">
+      <div
+        draggable="true"
+        class="bg-red-500 py-1 px-3 inline-block text-white text-sm rounded-xl mb-4"
+      >
+        Klassisch
+      </div>
+
+      <h2 class="text-2xl">Deutsche Meisterschaft der Amateure</h2>
+      <ul class="mt-4 mb-6 font-thin">
+        <li>München</li>
+        <li>9 Runden</li>
+        <li>2h 40/30 min rest</li>
+      </ul>
+      <div class="flex justify-between">
+        <button
+          type="button"
+          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        >
+          Mehr anzeigen
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup></script>
+
+<style></style>
