c6da9c58bdef662c0bde4800091082acf3049163
wertzuk <pascalb95@gmx.de>
2023-10-18 13:00:10 +0200

    link event card to detail page

--- src/components/Event.vue
+++ src/components/Event.vue
@@ -22,16 +22,15 @@
         <li>{{ event.time }}</li>
       </ul>
       <div class="flex justify-between">
-        <button
-          type="button"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+        <router-link
+          :to="`/events/${event.id}`"
+          class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2"
         >
           Mehr anzeigen
-        </button>
+        </router-link>
       </div>
     </div>
   </div>
-  <div v-else>Loading...</div>
 </template>
 
 <script setup>
