80f20219f9adc4438d36e7caa94985dccf2b80b1
wertzuk <pascalb95@gmx.de>
2023-11-26 13:53:28 +0100

    create primary button component

---
+++ resources/js/Pages/Components/ButtonPrimary.vue
@@ -0,0 +1,14 @@
+<template>
+    <a
+        :href="href"
+        class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-5 py-2 mr-2 mb-2"
+    >
+        <slot />
+    </a>
+</template>
+
+<script setup>
+defineProps({ href: String });
+</script>
+
+<style lang="scss" scoped></style>
