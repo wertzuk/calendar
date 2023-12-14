71737ee9c3d96c95a499fd14751ab8c42346e285
wertzuk <pascalb95@gmx.de>
2023-12-14 23:45:29 +0100

    fill Show.vue with data

--- resources/js/Pages/Tournament/Show.vue
+++ resources/js/Pages/Tournament/Show.vue
@@ -1,12 +1,61 @@
 <template>
-    <a href="/tournaments" class="border">Zurück</a>
+    <ButtonLink href="/tournaments">Zurück</ButtonLink>
 
-    <div>
-        <div v-for="(item, key) in tournament" :key="key">
-            <span>{{ key }}</span>
-            <span class="ml-8 text-sm">{{ item }}</span>
+    <h1 class="my-8 text-3xl text-gray-900 dark:text-gray-100">
+        {{ tournament.name }}
+    </h1>
+    <dl
+        class="max-w-md text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700"
+    >
+        <div class="flex flex-col pb-3">
+            <DescriptionTitle>Zeitkontrolle</DescriptionTitle>
+            <DescriptionDetails>{{
+                tournament.time_control
+            }}</DescriptionDetails>
         </div>
-    </div>
+        <div class="flex flex-col py-3">
+            <DescriptionTitle>Kategorie</DescriptionTitle>
+            <DescriptionDetails>{{ tournament.chess_type }}</DescriptionDetails>
+        </div>
+        <div class="flex flex-col py-3">
+            <DescriptionTitle>Datum</DescriptionTitle>
+            <DescriptionDetails>
+                <span>{{
+                    convertDateFormatToDDMMYYYY(tournament.start_date)
+                }}</span>
+                <span
+                    v-if="
+                        tournament.end_date &&
+                        tournament.end_date !== tournament.start_date
+                    "
+                >
+                    -
+                    {{ convertDateFormatToDDMMYYYY(tournament.end_date) }}</span
+                ></DescriptionDetails
+            >
+        </div>
+
+        <div class="flex flex-col py-3">
+            <DescriptionTitle>Ort</DescriptionTitle>
+            <DescriptionDetails>{{ tournament.city }}</DescriptionDetails>
+        </div>
+        <div class="flex flex-col py-3" v-if="tournament.number_of_rounds">
+            <DescriptionTitle>Anzahl der Runden</DescriptionTitle>
+            <DescriptionDetails>{{
+                tournament.number_of_rounds
+            }}</DescriptionDetails>
+        </div>
+        <div class="flex flex-col py-3" v-if="tournament.format">
+            <DescriptionTitle>Turniersystem</DescriptionTitle>
+            <DescriptionDetails>{{ tournament.format }}</DescriptionDetails>
+        </div>
+        <div class="flex flex-col py-3" v-if="tournament.starting_fee">
+            <DescriptionTitle>Startgeld</DescriptionTitle>
+            <DescriptionDetails>{{
+                tournament.starting_fee
+            }}</DescriptionDetails>
+        </div>
+    </dl>
 </template>
 
 <script>
@@ -14,9 +63,27 @@ export default { layout: Layout };
 </script>
 
 <script setup>
+import ButtonLink from "../Components/ButtonLink.vue";
+import DescriptionTitle from "../Components/DescriptionTitle.vue";
+import DescriptionDetails from "../Components/DescriptionDetails.vue";
 import Layout from "../Layout.vue";
 
 const props = defineProps({ tournament: Object });
+
+// TODO: Make globally available
+function convertDateFormatToDDMMYYYY(oldDate) {
+    const date = new Date(oldDate);
+    if (!date || isNaN(date)) {
+        return null;
+    }
+
+    return `${addLeadingZero(date.getDate())}.${addLeadingZero(
+        date.getMonth() + 1
+    )}.${date.getFullYear()}`;
+}
+function addLeadingZero(n) {
+    return n < 10 ? "0" + n : n;
+}
 </script>
 
 <style lang="scss" scoped></style>
