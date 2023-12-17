1a5a20565b23b84f5fed27c0dae844146a40ac18
wertzuk <pascalb95@gmx.de>
2023-12-17 21:57:48 +0100

    add title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     @inertiaHead
+    <title>Turnierkalender</title>
   </head>
   <body class="bg-white dark:bg-gray-900 min-h-screen">
     @inertia
