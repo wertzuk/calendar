6456c84be6b2e3b549d18f28a62eecd56312d3e0
wertzuk <pascalb95@gmx.de>
2023-12-21 19:36:24 +0100

    add seeder for tournament table

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,11 +12,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        \App\Models\Tournament::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $this->call([
+            TournamentSeeder::class,
+        ]);
     }
 }
