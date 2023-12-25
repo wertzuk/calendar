655908ae00b5e2ced68eeb08dd0c42d84bd3513a
wertzuk <pascalb95@gmx.de>
2023-12-25 20:15:21 +0100

    add close icon

---
+++ resources/js/Pages/Components/Icons/IconClose.vue
@@ -0,0 +1,21 @@
+<template>
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+    >
+        <path
+            fill="none"
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M2 30L30 2m0 28L2 2"
+        />
+    </svg>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped></style>
